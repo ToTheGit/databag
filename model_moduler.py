6e48ae39f225b912e5a461a382c612b58ba45345
NAMWONSHIK <nws717@naver.com>
2023-05-30 22:08:24 +0900

    다중 출력 구현

--- model_moduler.py
+++ model_moduler.py
@@ -76,36 +76,34 @@ class Preprocessing:
         
         data = pd.get_dummies(data, columns=['PRECPT_TYPE', 'day_of_week', 'is_holiday']).astype('int32')
         
-        # Calculate mean and deviation of each row
-        data['MEAN'] = data[['AREA_PPLTN_MIN', 'AREA_PPLTN_MAX']].mean(axis=1)
-        data['DEV'] = data['MEAN'] - data[['AREA_PPLTN_MIN', 'AREA_PPLTN_MAX']].min(axis=1)
-
-        # Normalize MEAN and DEV columns
-        self.mean = data[['MEAN', 'DEV']].mean()
-        self.std = data[['MEAN', 'DEV']].std()
-
-        data[['MEAN', 'DEV']] = (data[['MEAN', 'DEV']] - self.mean) / self.std
-        min_max = data[['AREA_PPLTN_MIN', 'AREA_PPLTN_MAX']]
-        data = data.drop(['AREA_PPLTN_MIN', 'AREA_PPLTN_MAX'], axis=1)
-        return data, min_max
+        self.mean = data[['AREA_PPLTN_MIN', 'AREA_PPLTN_MAX']].mean(axis=0)
+        data[['AREA_PPLTN_MIN', 'AREA_PPLTN_MAX']] -= self.mean
+        self.std = data[['AREA_PPLTN_MIN', 'AREA_PPLTN_MAX']].std(axis=0)
+        data[['AREA_PPLTN_MIN', 'AREA_PPLTN_MAX']] /= self.std
+        
+        target1 = data['AREA_PPLTN_MIN'].values
+        target2 = data['AREA_PPLTN_MAX'].values
+        targets = np.stack((target1, target2), axis=-1)
+        return data, targets
 
 
 # In[ ]:
 
 
 class Modeling:
-    def __init__(self, preprocessor, data, delay=2304, sequence_length=2016, sampling_rate=1, batch_size=64):
+    def __init__(self, preprocessor, data, targets, delay=2304, sequence_length=2016, sampling_rate=1, batch_size=64):
         self.preprocessor = preprocessor
         self.data = data
         self.delay = delay
+        self.targets = targets
         self.sequence_length = sequence_length
         self.sampling_rate = sampling_rate
         self.batch_size = batch_size
 
-    def create_dataset(self, target_column):
+    def create_dataset(self):
         dataset = keras.preprocessing.timeseries_dataset_from_array(
             self.data[:-self.delay],
-            targets=self.data[target_column][self.delay:],
+            targets= self.targets[self.delay:],
             sampling_rate=self.sampling_rate,
             sequence_length=self.sequence_length,
             shuffle=False,
@@ -113,46 +111,59 @@ class Modeling:
         )
         return dataset
 
-    def modeling(self, dataset, target_column):
-        inputs = keras.Input(shape=(self.sequence_length, self.data.shape[-1]))
+    def modeling(self, dataset):
+        inputs = keras.Input(shape=(sequence_length, data.shape[-1]))
+        # Shared layers
         x = layers.GRU(32, return_sequences=True)(inputs)
         x = layers.GRU(32)(x)
-        outputs = layers.Dense(1)(x)
-        model = keras.Model(inputs, outputs)
-        model.compile(optimizer="Adam", loss="mse", metrics=["mae"])
-        history = model.fit(dataset, epochs=10)
+        # Output layers
+        output1 = layers.Dense(1, name='output1')(x)  # Output layer for first target
+        output2 = layers.Dense(1, name='output2')(x)  # Output layer for second target
+        model = keras.Model(inputs, [output1, output2])
+        model.compile(optimizer="Adam", loss={"output1": "mse", "output2": "mse"}, 
+                      metrics={"output1": ["mae"], "output2": ["mae"]}, )
+        history = model.fit(dataset, epochs=20)
         return model
             
-    def prediction(self, model, sequence_length, target):
+    def prediction(self, model, sequence_length):
         input_sequence = self.data[-sequence_length:].values
         num_features = self.data.shape[1]
-        predictions = []  # list to hold our predictions
+        predictions1 = []  # list to hold our predictions for output1
+        predictions2 = []  # list to hold our predictions for output2
 
-        # Predict the population for the next 24 hours
+        # Predict for the next 24 hours
         for _ in range(sequence_length):
-            # Reshape the sequence to match the model_max's input shape
+            # Reshape the sequence to match the model's input shape
             input_sequence_reshaped = input_sequence.reshape((1, sequence_length, -1))
 
             # Make a prediction for the next interval
-            next_population = model.predict(input_sequence_reshaped)
+            next_output = model.predict(input_sequence_reshaped)
 
             # Append the predicted value to the end of the sequence in the last column
             input_sequence = np.roll(input_sequence, shift=-1, axis=0)  # shift all rows up
-            
-            input_sequence[-1, -1] = next_population[0, 0]
+
+            input_sequence[-1, -1] = next_output[0]  # this should be adjusted based on the structure of your data
+            input_sequence[-1, -2] = next_output[1]  # this should be adjusted based on the structure of your data
+
             # Save the prediction
-            predictions.append(next_population[0, 0])  # save the prediction for this step
-                
-        # Convert list to numpy array for easier manipulation
-        predictions = np.array(predictions)
-        predicted_values = predictions * self.preprocessor.std[target] + self.preprocessor.mean[target]
+            predictions1.append(next_output[0])  # save the prediction for output1
+            predictions2.append(next_output[1])  # save the prediction for output2
+
+        # Convert lists to numpy arrays for easier manipulation
+        predictions1 = np.array(predictions1)
+        predictions2 = np.array(predictions2)
+        predicted_values = {}
+        predicted_values['AREA_PPLTN_MIN'] = predictions1 * self.preprocessor.std['AREA_PPLTN_MIN'] + self.preprocessor.mean['AREA_PPLTN_MIN']
+        predicted_values['AREA_PPLTN_MAX'] = predictions2 * self.preprocessor.std['AREA_PPLTN_MAX'] + self.preprocessor.mean['AREA_PPLTN_MAX']
         return predicted_values
     
-    def get_ready(self, df_predictions, min_max):
+    def get_ready(self, df_predictions):
         df_predictions = pd.DataFrame(df_predictions)
-        df_predictions['AREA_PPLTN_MAX'] = df_predictions['MEAN'] + df_predictions['DEV']
-        df_predictions['AREA_PPLTN_MIN'] = df_predictions['MEAN'] - df_predictions['DEV']
-        df_predictions = df_predictions.drop(['MEAN', 'DEV'], axis=1)
+        def swap_min_max(row):
+            if row['AREA_PPLTN_MIN'] > row['AREA_PPLTN_MAX']:
+                row['AREA_PPLTN_MIN'], row['AREA_PPLTN_MAX'] = row['AREA_PPLTN_MAX'], row['AREA_PPLTN_MIN']
+            return row
+        df_predictions = df_predictions.apply(swap_min_max, axis=1)
         
         df_predictions[['AREA_PPLTN_MIN', 'AREA_PPLTN_MAX']] = df_predictions[[
     'AREA_PPLTN_MIN', 'AREA_PPLTN_MAX']].astype('int64')
